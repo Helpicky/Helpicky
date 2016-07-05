1bbeefdaff4b7ba79a741235c2dcec7726bb7789
kevin6983876 <kevin6983876@gmail.com>
2016-07-05 21:11:19 +0800

    fix MainActivity.java from line 118 to 128

--- app/src/main/java/tk/helpicky/helpicky/MainActivity.java
+++ app/src/main/java/tk/helpicky/helpicky/MainActivity.java
@@ -115,17 +115,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.settings) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.diary) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.recommendation) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.achievement) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.communicate) {
 
         }
 
